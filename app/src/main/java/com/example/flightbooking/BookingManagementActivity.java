289713c7d3fef648a7714694e1d02027c7725230
pvTien4970 <tien97514@gmail.com>
2024-08-20 06:13:33 +0700

    Done UI Booking management

--- app/src/main/java/com/example/flightbooking/BookingManagementActivity.java
+++ app/src/main/java/com/example/flightbooking/BookingManagementActivity.java
@@ -8,17 +8,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.flightbooking.databinding.ActivityBookingManagementBinding;
+
 public class BookingManagementActivity extends AppCompatActivity {
+    ActivityBookingManagementBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_booking_management);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityBookingManagementBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        
+        addEvents();
+    }
+
+    private void addEvents() {
     }
 }
